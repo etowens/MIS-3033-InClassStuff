f08b6292e8848698cbe2737a1467524e7cb78b83
Evan Owens <Evan.T.Owens-1@ou.edu>
2019-08-26 11:37:42 -0500

    Completed Problem 2

---
+++ GitHub/Problem2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Students = new Dictionary<int, string>();
+
+            Students.Add(1234567, "Evan Owens");
+            
+            if(Students.ContainsKey(1234567))
+            {
+                Console.WriteLine($"{1234567} already exists");
+                Students[1234567] = "Katie";
+            }
+            else
+            {
+                Console.WriteLine("Key doesn't exist");
+            }
+            Console.WriteLine(Students[1234567]);
+            Console.ReadKey();
+        }
+    }
+}
