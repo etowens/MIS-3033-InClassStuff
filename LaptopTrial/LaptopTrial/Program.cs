89f48f7f628722f690cfb6fad39401c807e4e7d3
Evan Owens <Evan.T.Owens-1@ou.edu>
2019-09-02 10:50:47 -0500

    check if goes onto github

---
+++ LaptopTrial/LaptopTrial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaptopTrial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Just trying from my laptop before I start my homework.");
+            Console.ReadKey();
+        }
+    }
+}
